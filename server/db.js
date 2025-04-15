dfc069959b7df6041fbe153122f94b6c96db11dc
Glitch (yysun-apprun-websockets-sqlite) <none>
2025-04-15 17:52:21 +0000

    🌲👛 Checkpoint ./server/db.js:77655234/508

--- server/db.js
+++ server/db.js
@@ -16,11 +16,11 @@ console.log('Using database: ', dbFile);
 const db = new sqlite3.Database(dbFile);
 db.serialize(() => {
   if (!exists) {
+    // TODO: To add memberIds.
     db.run(`CREATE TABLE Activities (
                 id          INTEGER PRIMARY KEY AUTOINCREMENT,
                 name        TEXT    NOT NULL,
                 ownerId     TEXT    NOT NULL,
-                memberIds:  Text    NOT NULL,
                 started     NUMERIC NOT NULL DEFAULT 0,
               );`);
     console.log("New table created!");
@@ -39,10 +39,22 @@ db.serialize(() => {
 
 app.on('@get-all-activities', (json, ws) => {
   using(db => {
-    const sql = 'select * from todo';
+    const sql = 'select * from activities';
     db.all(sql, function (err, rows) {
       json.state = rows || [];
       console.log('  >', json);
+      ws.send(JSON.stringify(json));
+    });
+  });
+});
+
+app.on('@create-activity', (json, ws) => {
+  using(db => {
+    const sql = 'insert into activities (name, ownerId) values (?,?)';
+    db.run(sql, json.state.title, json.state.done, json.ip, function () {
+      json.state.id = this.lastID;
+      json.state.ip = json.ip;
+      console.log('  >', 'created', json);
       ws.send(JSON.stringify(json));
     });
   });
